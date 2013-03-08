8b4bef6c2b42cbee98332d2b2be1da98f1866a78
Eric Liu <Eric@res2net-129-97-232-42.dynamic.uwaterloo.ca>
2013-03-08 18:53:32 -0500

    Add comment to barn1

--- sec1.3/barn1/barn1.cpp
+++ sec1.3/barn1/barn1.cpp
@@ -17,8 +17,8 @@ int main(){
 
 	int M, S, C;
 	int tmp;
-	vector <int> stalls;
-	vector <int> spareStalls;
+	vector <int> stalls; // Store stall numbers
+	vector <int> spareStalls; // Store number of spare stalls that is continuous
 
 	fin >> M >> S >> C;
 
@@ -27,20 +27,21 @@ int main(){
 		stalls.push_back(tmp);
 	}
 
-	sort(stalls.begin(), stalls.end());
+	sort(stalls.begin(), stalls.end());  // Sort then count continuous idle stalls
 	
 	for (vector<int>::iterator it=stalls.begin()+1; it!=stalls.end(); it++){
 		tmp = *it - *(it-1);
-		if (tmp > 1)
+		if (tmp > 1) // If the number of idle stalls between two occupied stalls is 1 or more
 			spareStalls.push_back(tmp-1);
 	}
 
 	
-	sort(spareStalls.begin(), spareStalls.end(), std::greater<int>());
+	sort(spareStalls.begin(), spareStalls.end(), std::greater<int>());  // Sort numbers of continuous stalls 
 	
 	int result = stalls.back() - stalls.at(0)+1; // Original total number of stalls blocked
 	M--; // Use one board to block all stalls
 
+	// Greedy! Choose the longest continuous stalls first!
 	for (vector<int>::iterator it=spareStalls.begin(); it!=spareStalls.end(); it++){
 		if (M<=0 || result == 0) break;
 		result -= *it;
